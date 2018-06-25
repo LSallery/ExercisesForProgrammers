ae951d4c46d9dc2454dedee95638e232db7abe6b
Lydia <lydia@Fry.local>
2018-06-25 23:20:28 +0100

    First commit

---
+++ src/main/scala-2.12/HelloWorld.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by lydia on 17/06/2018.
+  */
+
+import scala.io.StdIn._
+
+object HelloWorld extends App {
+  println("Hello, world!")
+
+  val name = readLine("What's your name? ")
+  print("Hello, " + name + "\nYour name has " + name.length + " characters.")
+
+//  val quote =
+}
